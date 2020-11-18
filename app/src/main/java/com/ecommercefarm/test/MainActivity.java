b60e0fccb1db215b0c1c1c45d4c3f8d26eab3b88
Mario <68830236+BecerraDesarrollo@users.noreply.github.com>
2020-11-18 19:42:25 +0100

    Update MainActivity.java

--- app/src/main/java/com/ecommercefarm/test/MainActivity.java
+++ app/src/main/java/com/ecommercefarm/test/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.ecommercefarm.test.data.Film;
 import com.ecommercefarm.test.data.FilmList;
-import com.ecommercefarm.test.data.FirebaseData;
 import com.ecommercefarm.test.data.RemoteList;
 
 import androidx.appcompat.app.AppCompatActivity;
