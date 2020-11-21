5fdccf9105b802be640237e2c2d149e52212cd98
Mario <68830236+BecerraDesarrollo@users.noreply.github.com>
2020-11-21 15:19:01 +0100

    second final commit

--- app/src/main/java/com/ecommercefarm/test/MainActivity.java
+++ app/src/main/java/com/ecommercefarm/test/MainActivity.java
@@ -13,6 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        FirebaseData data=new FirebaseData();
+
+        data.setActionListeners(new FirebaseData.FireBaseDataEvent() {
+            @Override
+            public void loaded() {
+                //Se ignora
+            }
+
+            @Override
+            public void canceled() {
+                //Se ignora
+            }
+
+            @Override
+            public void maintenance(boolean is) {
+                if(is){
+                    setContentView(R.layout.maintenance_layout);
+                }else{
+                    setContentView(R.layout.activity_main);
+                }
+            }
+        });
+
+        data.service();
+
         Log.i("MainActivity","Created");
     }
 }
